f0d3c2bc3441ba697a4613c171ea195c2364c354
nicolodallaglio <dallaglionicol@gmail.com>
2024-02-29 20:16:58 +0100

    ok

--- djangoapp/djangoapp/deployment.py
+++ djangoapp/djangoapp/deployment.py
@@ -32,11 +32,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
-STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
+#STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+#STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
 
+STATIC_URL = os.environ.get(BASE_DIR, "/static/")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #cosmosDB
 DATABASES = {
