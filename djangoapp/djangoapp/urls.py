2b5afd63eb6837e8eea864e0de5358f29f64178c
nicolodallaglio <dallaglionicol@gmail.com>
2024-02-29 03:28:53 +0100

    ok

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppIoT.views import DatiSerialeView, Ranking, SaveDataView, create_my_model, index
+from AppIoT.views import DatiSerialeView, Ranking, SaveDataView, create_my_model, index, home
 
 urlpatterns = [
+    path('', home, name='home'),
     path("admin/", admin.site.urls),
     path('create_my_model/', create_my_model, name='create_my_model'),
     path('dati-seriale/', DatiSerialeView.as_view(), name='dati-seriale'),
