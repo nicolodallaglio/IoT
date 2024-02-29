bcda7b82659c2c011d5728c6330815af56335bcd
nicolodallaglio <dallaglionicol@gmail.com>
2024-02-29 16:23:53 +0100

    ok

--- djangoapp/djangoapp/deployment.py
+++ djangoapp/djangoapp/deployment.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #cosmosDB
 DATABASES = {
