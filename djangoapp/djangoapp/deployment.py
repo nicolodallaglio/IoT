b1d3f837eb696cf2e7e97f3174bdac30f3b044cd
nicolodallaglio <dallaglionicol@gmail.com>
2024-03-10 13:38:50 +0100

    test

--- djangoapp/djangoapp/deployment.py
+++ djangoapp/djangoapp/deployment.py
@@ -37,11 +37,11 @@ MIDDLEWARE = [
 #STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 #STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
 
-STATIC_URL = os.environ.get(BASE_DIR, "staticfiles")
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#STATIC_URL = os.environ.get(BASE_DIR, "static")
+#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 #DBforPostgreSQL
